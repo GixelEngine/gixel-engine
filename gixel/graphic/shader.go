9fe1f205f2d2ccee2d5f080b8fbbf0ff2a910aed
Oded Rosenblatt <odedro987@gmail.com>
2023-06-14 17:40:55 +0300

    init shader

---
+++ gixel/graphic/shader.go
@@ -0,0 +1,41 @@
+package graphic
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type BaseGxlShader struct {
+	shader *ebiten.Shader
+	opts   *ebiten.DrawRectShaderOptions
+}
+
+func NewShader(src []byte, opts *ebiten.DrawRectShaderOptions) *BaseGxlShader {
+	s, err := ebiten.NewShader(src)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	return &BaseGxlShader{
+		shader: s,
+		opts:   opts,
+	}
+}
+
+func (s *BaseGxlShader) Shader() *ebiten.Shader {
+	return s.shader
+}
+
+func (s *BaseGxlShader) Opts() *ebiten.DrawRectShaderOptions {
+	return s.opts
+}
+
+func (s *BaseGxlShader) Update(elapsed float64) {
+}
+
+type GxlShader interface {
+	Shader() *ebiten.Shader
+	Opts() *ebiten.DrawRectShaderOptions
+	Update(elapsed float64)
+}
--- gixel/sprite.go
+++ gixel/sprite.go
@@ -14,6 +14,8 @@ type BaseGxlSprite struct {
 	frameIdx int
 	color    color.RGBA // TODO: Think of a better name
 	drawOpts *ebiten.DrawImageOptions
+	shader   graphic.GxlShader
+	geom     ebiten.GeoM
 }
 
 func (s *BaseGxlSprite) Init(game *GxlGame) {
@@ -34,8 +36,16 @@ func (s *BaseGxlSprite) ApplyGraphic(graphic *graphic.GxlGraphic) {
 	s.SetSize(graphic.Size())
 }
 
+func (s *BaseGxlSprite) ApplyShader(shader graphic.GxlShader) {
+	s.shader = shader
+}
+
 func (s *BaseGxlSprite) Graphic() *graphic.GxlGraphic {
 	return s.graphic
+}
+
+func (s *BaseGxlSprite) Shader() graphic.GxlShader {
+	return s.shader
 }
 
 func (s *BaseGxlSprite) FrameIdx() *int {
@@ -65,19 +75,25 @@ func (s *BaseGxlSprite) Draw() {
 		return
 	}
 
-	s.drawOpts.GeoM.Reset()
+	s.geom.Reset()
 	sxy := s.ScreenPosition()
 	w, h := s.graphic.Size()
-	s.drawOpts.GeoM.Translate(float64(-w/2), float64(-h/2))
-	s.drawOpts.GeoM.Rotate(s.angle * s.angleMultiplier)
-	s.drawOpts.GeoM.Scale(s.scale.X*s.scaleMultiplier.X, s.scale.Y*s.scaleMultiplier.Y)
-	s.drawOpts.GeoM.Translate(float64(w/2), float64(h/2))
-	s.drawOpts.GeoM.Translate(sxy.X, sxy.Y)
-	// // TODO: Add color for tinting/etc
-	s.drawOpts.ColorM.Reset()
-	s.drawOpts.ColorM.ScaleWithColor(s.color)
+	s.geom.Translate(float64(-w/2), float64(-h/2))
+	s.geom.Rotate(s.angle * s.angleMultiplier)
+	s.geom.Scale(s.scale.X*s.scaleMultiplier.X, s.scale.Y*s.scaleMultiplier.Y)
+	s.geom.Translate(float64(w/2), float64(h/2))
+	s.geom.Translate(sxy.X, sxy.Y)
 
-	s.camera.Screen().DrawImage(s.graphic.GetFrame(s.frameIdx), s.drawOpts)
+	if s.shader != nil {
+		s.shader.Opts().GeoM = s.geom
+		s.camera.Screen().DrawRectShader(s.w, s.h, s.shader.Shader(), s.shader.Opts())
+	} else {
+		s.drawOpts.GeoM = s.geom
+		// // TODO: Add color for tinting/etc
+		s.drawOpts.ColorM.Reset()
+		s.drawOpts.ColorM.ScaleWithColor(s.color)
+		s.camera.Screen().DrawImage(s.graphic.GetFrame(s.frameIdx), s.drawOpts)
+	}
 	// TODO: This currently prevents draw call batching, consider drawing in a separate run
 	s.game.Debug().Collision.DrawBounds(s.camera.Screen(), *math.NewRectangle(sxy.X+s.offset.X, sxy.Y+s.offset.Y, float64(s.w), float64(s.h)))
 }
@@ -85,6 +101,7 @@ func (s *BaseGxlSprite) Draw() {
 type GxlSprite interface {
 	GxlObject
 	ApplyGraphic(graphic *graphic.GxlGraphic)
+	ApplyShader(shader graphic.GxlShader)
 	Graphic() *graphic.GxlGraphic
 	FrameIdx() *int
 	Color() *color.RGBA
